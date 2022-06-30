a2d58509001affc10b105e5678928649732620b2
Matheus R <matheusrbarros03@gmail.com>
2022-06-30 12:56:17 -0300

    - handlers.py: adicionadas classes de handler de Produto, Colaborador e Venda - requirements.txt atualizado

--- api/handlers.py
+++ api/handlers.py
@@ -1,5 +1,6 @@
 import sys
 import falcon
+from falcon.media.validators import jsonschema as jsonschema
 
 if __name__ == "__main__":
     print("Este arquivo é um módulo, e portanto não deve ser executado.")
@@ -14,8 +15,6 @@ class Handler(object):
     def __init__(self, database):
         self.db = database
 
-# ideia kibada COMPLETAMENTE do Borges:
-# a API SE AUTODOCUMENTA
 class Help(Handler):
     desc = "Classe para documentação da API."
     usage = "GET retorna informações sobre todos os handlers."
@@ -33,4 +32,60 @@ class Help(Handler):
     def on_get(self, req, resp):
         resp.media = self.instances
 
-class 
+class Colaborador(Handler):
+    desc = "Classe para manipulações na tabela Colaborador."
+    usage = "GET retorna todos, POST cria um"
+    route = "/colaborador"
+
+    post_schema = {
+        "type": "object",
+        "title": "Inserção de Colaborador via POST.",
+        "description": "Informação de um Colaborador a ser adicionada.",
+        # TODO: property
+    }
+
+    def on_get(self, req, resp):
+        resp.media = self.db.get_colaboradores()
+
+    # TODO
+    def on_post(self, req, resp):
+        pass
+
+class Produto(Handler):
+    desc = "Classe para manipulações na tabela Produto."
+    usage = "GET retorna todos, POST cria um"
+    route = "/produto"
+
+    post_schema = {
+        "type": "object",
+        "title": "Criação de um Produto no banco de dados via POST."
+        "properties": {
+            "id_produto": {
+                "type": "integer",
+                "description": "Identificação única do produto no estoque",
+                "length": 6
+            },
+            "nome": {
+                "type": "string",
+                "description": "O nome do produto",
+                "minLength": 1
+            },
+            "preco": {
+                "type": "money",
+                "description": "O preço associado ao produto",
+            },
+        }
+        "required": ["id_produto", "nome", "preco"]
+    }
+
+    def on_get(self, req, resp):
+        resp.media = self.db.get_produtos()
+
+    # TODO
+    def on_post(self, req, resp):
+        pass
+
+class Venda(Handler):
+    desc = "Classe para manipulações na tabela Venda."
+    usage = "GET retorna todas, POST cria uma, PUT atualiza uma"
+    route = "/produto"
