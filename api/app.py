f34806cc9512bb6e79be9fb983ffa38e9e06af1d
Matheus R <matheusrbarros03@gmail.com>
2022-07-20 12:11:42 -0300

    app: adicionadas rotas para as tabelas

--- api/app.py
+++ api/app.py
@@ -1,3 +1,10 @@
 import falcon
 
-class 
+import handlers, banco
+
+db = banco.Banco("/tmp/tmp.hfU0gYjhh9/banco.db", host="0.0.0.0", port=5432)
+
+app = falcon.App()
+app.add_route('/colaborador', handlers.Colaborador())
+app.add_route('/produto', handlers.Produto())
+app.add_route('/venda', handlers.Venda())
